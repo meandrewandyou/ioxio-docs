bb259c5365614b4ae76a85d1132d888d561e85f8
Joakim Nordling <joakim.nordling@ioxio.com>
2024-10-28 16:48:21 +0200

    Bunch of fixes to verifying id_token

--- src/routes/guides/verifying-id-token/+page.svelte
+++ src/routes/guides/verifying-id-token/+page.svelte
@@ -5,9 +5,11 @@
   import Breadcrumbs from "$lib/components/Breadcrumbs.svelte"
   import Title from "$lib/components/Title.svelte"
   import TableOfContents from "$lib/components/TableOfContents.svelte"
+  import type { PageData } from "../../../../.svelte-kit/types/src/routes/guides/building-an-application/$types"
+  import A from "$lib/components/A.svelte"
 
   /** @type {import('./$types').PageData} */
-  export let data
+  export let data: PageData
 </script>
 
 <TableOfContents>
@@ -22,17 +24,17 @@
     logging purposes.
   </p>
   <p>
-    When the user logs in to an application on the Dataspace, the application receives an id_token
+    When the user logs in to an application on the Dataspace, the application receives an <em
+      >id_token</em
+    >
     for the user. The application can use this as a Bearer Token when communicating with the product
-    gateway by adding an Authorization HTTP header with a value of the form Bearer
-    <id_token
-      >. The Authorization header is forwarded by the product gateway to the data source or
-      productizer.
-    </id_token>
+    gateway by adding an <em>Authorization</em> HTTP header with a value of the form
+    <em>Bearer &lt;id_token&gt;</em>. The <em>Authorization</em> header is forwarded by the product gateway
+    to the data source or productizer.
   </p>
   <p>
-    The id_token is a<a href="https://datatracker.ietf.org/doc/html/rfc7519" target="_blank">
-      JSON Web Token (JWT)</a
+    The <em>id_token</em> is a <A href="https://datatracker.ietf.org/doc/html/rfc7519">
+      JSON Web Token (JWT)</A
     >
     that can be decoded and verified. This guide will explain in more detail the process of extracting,
     decoding and verifying it.
@@ -40,62 +42,57 @@
   <SectionTitle title="Verifying the id_token in the productizer" />
   <h3>Get the Authorization header</h3>
   <p>
-    The productizer or data source needs to first extract the id_token from the Authorization HTTP
-    header of the request. Please note that the header is in many data source definitions optional,
-    so take that in mind when parsing it.
+    The productizer or data source needs to first extract the <em>id_token</em> from the
+    <em>Authorization</em> HTTP header of the request. Please note that the header is in many data source
+    definitions optional, so take that in mind when parsing it.
   </p>
   <h3>Extracting the id_token from the header</h3>
   <p>
-    The value of the header should be of the form Bearer
-    <token
-      >, where
-      <token> is the actual id_token. </token></token
-    >
+    The value of the header should be of the form <em>Bearer &lt;token&gt;</em>, where
+    <em>&lt;token&gt;</em> is the actual <em>id_token</em>.
   </p>
   <p>
     You can split the header at the space character or the right string index and verify the scheme
-    is Bearer and followed by a space character. <a
-      href="https://datatracker.ietf.org/doc/html/rfc6750#section-2.1"
-      target="_blank">RFC 6750 section 2.1</a
-    > states the scheme is Bearer, but some frameworks in practice do a case insensitive match to improve
-    compatibility with some client libraries that don't fully comply with the standard.
+    is <em>Bearer</em> and followed by a space character. <A
+      href="https://datatracker.ietf.org/doc/html/rfc6750#section-2.1">RFC 6750 section 2.1</A
+    > states the scheme is <em>Bearer</em>, but some frameworks in practice do a case insensitive
+    match to improve compatibility with some client libraries that don't fully comply with the
+    standard.
   </p>
   <p>
-    Some frameworks also have built in support for extracting the Bearer Token. For example FastAPI,
-    where the usage is actually best illustrated in the <a
+    Some frameworks also have built in support for extracting the Bearer Token. For example <A
+      href="https://fastapi.tiangolo.com/">FastAPI</A
+    >, where the usage is actually best illustrated in the <A
       href="https://github.com/tiangolo/fastapi/blob/0.71.0/tests/test_security_http_bearer.py#L7-L12"
-      target="_blank"
     >
-      HTTPBearer test cases</a
+      HTTPBearer test cases</A
     >. However you would likely want to adapt that slightly to make it an optional dependency for
     easier reuse.
   </p>
   <h3>Reading the id_token</h3>
   <p>
-    The id_token is a <a href="https://datatracker.ietf.org/doc/html/rfc7519" target="_blank"
-      >JSON Web Token (JWT)</a
+    The <em>id_token</em> is a <A href="https://datatracker.ietf.org/doc/html/rfc7519"
+      >JSON Web Token (JWT)</A
     >.
   </p>
   <p>
-    If you are unfamiliar with JWTs you might want to check out the <a
-      href="https://jwt.io/introduction"
-      target="_blank">JSON Web Token Introduction.</a
+    If you are unfamiliar with JWTs you might want to check out the <A
+      href="https://jwt.io/introduction">JSON Web Token Introduction.</A
     >
     The same site has a really good tool for decoding JWTs directly in the web browser at
-    <a href="https://jtw.io" target="blank">jwt.io,</a>
+    <A href="https://jwt.io">jwt.io,</A>
     as well as a fairly good list of different
-    <a href="https://jwt.io/libraries" target="blank">JWT libraries</a> for different programming languages.
+    <A href="https://jwt.io/libraries">JWT libraries</A> for different programming languages.
   </p>
   <p>
     You most likely want to use one of the libraries for your programming language rather than try
-    to implement it according to the <a
-      href="https://datatracker.ietf.org/doc/html/rfc7519"
-      target="_blank">RFC 7519.</a
-    >
+    to implement it according to the <A href="https://datatracker.ietf.org/doc/html/rfc7519"
+      >RFC 7519</A
+    >.
   </p>
   <p>
-    Let's have a look at a valid example token from one of the applications on the sandbox Dataspace
-    and how to verify it.
+    Let's have a look at a valid example token from one of the applications on the IOXIO Sandbox
+    Dataspace and how to verify it.
   </p>
   <p>Let's examine this token:</p>
   <Code lang={json}>
@@ -112,19 +109,18 @@
   </p>
   <p>
     At the time you are reading this guide the token will unfortunately have expired, so some of the
-    verifications we will perform here won't work. However you can log in to the <a
-      href="https://mycompany.demos.ioxio.com/my-company/"
-      target="_blank">MyCompany Demo App</a
-    > and extract your own id_token from a cookie with that name once you've logged in.
+    verifications we will perform here won't work. However you can log in to the <A
+      href="https://mycompany.demos.ioxio.com/my-company/">MyCompany Demo App</A
+    > and extract your own <em>id_token</em> from a cookie with that name once you've logged in.
   </p>
   <p>This is what the content of the token will look like:</p>
   <img src="/view-jwt-token.png" alt="view of jwt token" />
   <p>
     Many JWT libraries also allow you to read the JWT without verifying it. In case your library for
     some reason does not do that, you can actually note that the token consists of three sections
-    separated by a dot, like AAA.BBB.CCC. The first section is the header, the second one the body
-    and the last one the signature. The first two are actually just Base64 encoded JSON strings that
-    can be easily decoded and read.
+    separated by a dot, like <em>AAA.BBB.CCC</em>. The first section (<em>AAA</em>) is the header,
+    the second one (<em>BBB</em>) is the body and the last one (<em>CCC</em>) is the signature. The
+    first two are actually just Base64 encoded JSON strings that can be easily decoded and read.
   </p>
   <SectionTitle title="Fetch the public key" />
   <p>
@@ -143,38 +139,37 @@
     purpose, as this might vary a lot.
   </p>
   <p>
-    In Python for example the <a
+    In Python for example the <A
       href="https://pyjwt.readthedocs.io/en/stable/usage.html#retrieve-rsa-signing-keys-from-a-jwks-endpoint"
-      target="_blank">PyJWT library</a
+      >PyJWT library</A
     >
-    has a PyJWKClient that can fetch the key if you already know the JWKS URI, but it does not support
-    asynchronous execution. There's however a separate library called
-    <a href="https://pypi.org/project/pyjwt-key-fetcher/" target="blank">pyjwt-key-fetcher</a> that supports
-    async and has built in caching and other features.
+    has a <em>PyJWKClient</em> that can fetch the key if you already know the JWKS URI, but it does
+    not support asynchronous execution. There's however a separate library called
+    <A href="https://pypi.org/project/pyjwt-key-fetcher/">pyjwt-key-fetcher</A> that supports async and
+    has built in caching and other features.
   </p>
   <h3>Find the JWKS URI</h3>
   <p>
-    The first step to find the key is to check the issuer of the token, which is found in iss field
-    in the body.
+    The first step to find the key is to check the issuer of the token, which is found in <em
+      >iss</em
+    > field in the body.
   </p>
   <p>
-    Please note that you should check that the iss is one you want to trust. In a typical IOXIO
-    Dataspace this would always be the address of the login portal of the Dataspace. In some cases
-    the Dataspace might use a limited set of different issuers, which should be listed and available
-    to developers of the Dataspace. Querying openid-configurations and JWKs from arbitrary issuers
-    and addresses should be avoided. The <a
-      href="https://pypi.org/project/pyjwt-key-fetcher/"
-      target="blank">pyjwt-key-fetcher</a
+    Please note that you should check that the <em>iss</em> is one you want to trust. In a typical
+    IOXIO Dataspace this would always be the address of the login portal of the Dataspace. In some
+    cases the Dataspace might use a limited set of different issuers, which should be listed and
+    available to developers of the Dataspace. Querying openid-configurations and JWKs from arbitrary
+    issuers and addresses should be avoided. The <A
+      href="https://pypi.org/project/pyjwt-key-fetcher/">pyjwt-key-fetcher</A
     > for example has the option to limit the fetching.
   </p>
   <p>
-    In this example the iss (Issuer) is <em> https://login.sandbox.ioxio-dataspace.com</em>, which
-    is the login portal of the sandbox Dataspace. The OpenID Connect configuration should be found
-    on the sub-path <em>/.well-known/openid-configuration</em>
-    relative to the iss. Thus in this case the the OpenID configuration can be fetched from
-    <a
-      href="https://login.sandbox.ioxio-dataspace.com/.well-known/openid-configuration"
-      target="_blank">https://login.sandbox.ioxio-dataspace.com/.well-known/openid-configuration</a
+    In this example the <em>iss</em> (Issuer) is <em>https://login.sandbox.ioxio-dataspace.com</em>,
+    which is the login portal of the IOXIO Sandbox Dataspace. The OpenID Connect configuration
+    should be found on the sub-path <em>/.well-known/openid-configuration</em>
+    relative to the <em>iss</em>. Thus in this case the the OpenID configuration can be fetched from
+    <A href="https://login.sandbox.ioxio-dataspace.com/.well-known/openid-configuration"
+      >https://login.sandbox.ioxio-dataspace.com/.well-known/openid-configuration</A
     >. At the time of writing this it returned this JSON content (formatted for readability):
   </p>
   <Code lang={json}>
@@ -199,12 +194,12 @@
 
 `}
   </Code>
-  <p>The jwks_uri specify where we can find the keys of this authentication provider.</p>
+  <p>The <em>jwks_uri</em> specify where we can find the keys of this authentication provider.</p>
   <h3>Find the key</h3>
   <p>
-    At the time of writing this, the jwks_uri <a
-      href="https://login.sandbox.ioxio-dataspace.com/api/oauth/jwks"
-      target="_blank">https://login.sandbox.ioxio-dataspace.com/api/oauth/jwks</a
+    At the time of writing this, the <em>jwks_uri</em>
+    <A href="https://login.sandbox.ioxio-dataspace.com/api/oauth/jwks"
+      >https://login.sandbox.ioxio-dataspace.com/api/oauth/jwks</A
     >
     returned this JSON content (formatted for readability):
   </p>
@@ -225,22 +220,27 @@
     `}
   </Code>
   <p>
-    Under keys there's a list of keys, each of which have a kid (Key ID) and alg (Algorithm). If we
-    look back at the decoded header of the JWT, we will notice that it also has a kid and alg.
-    Finding the right key object in the list of keys consists of finding a key with matching kid and
-    alg as well as validating that the use is set to sig which indicates signing. The login portal
-    on the IOXIO Dataspaces will in practice always use a unique kid for each alg, similar to most
-    other OpenID Connect providers and only hand out tokens signed with a key meant for signing
-    ("use": "sig"). When a key used by a login portal on an IOXIO Dataspace expires (after key
-    rotation) it's dropped from the list. In other words on the IOXIO Dataspaces using login
-    portals, it's enough to just match the kid to find the right key.
+    Under <em>keys</em> there's a list of keys, each of which have a <em>kid</em> (Key ID) and
+    <em>alg</em>
+    (Algorithm). If we look back at the decoded header of the JWT, we will notice that it also has a
+    <em>kid</em>
+    and <em>alg</em>. Finding the right key object in the list of <em>keys</em> consists of finding
+    a key with matching <em>kid</em> and
+    <em>alg</em> as well as validating that the <em>use</em> is set to <em>sig</em> which indicates
+    signing. The login portal on the IOXIO Dataspaces will in practice always use a unique
+    <em>kid</em>
+    for each <em>alg</em>, similar to most other OpenID Connect providers and only hand out tokens
+    signed with a key meant for signing (<em>"use": "sig"</em>). When a key used by a login portal
+    on an IOXIO Dataspace expires (after key rotation) it's dropped from the list. In other words on
+    the IOXIO Dataspaces using login portals, it's enough to just match the <em>kid</em> to find the
+    right key.
   </p>
   <h3>Caching</h3>
   <p>
     Please note that for performance reasons you should use some level of caching. The address of
-    the jwks_uri is for example not likely to change in practice. The keys are neither rotated
-    frequently, so they could be cached for some time, but it's still important they get refreshed
-    once in a while.
+    the <em>jwks_uri</em> is for example not likely to change in practice. The keys are neither rotated
+    frequently, so they could be cached for some time, but it's still important they get refreshed once
+    in a while.
   </p>
   <h3>Verifying the token signature</h3>
   <p>
@@ -254,18 +254,17 @@
   <h3>Verifying the fields</h3>
   <p>
     After verifying the signature, you should do some basic verification of some of the fields. Many
-    libraries by default verify the iat (Issued At) and exp (Expiration Time), but some do not
-    automatically do it. In that case you should verify that issued at is lees than or same as
-    current time, and current time is less than or same as expiration time. You might also want to
-    check the iss (Issuer) matches what you expect, but this would be good to check already before
-    fetching the keys.
+    libraries by default verify the <em>iat</em> (Issued At) and <em>exp</em> (Expiration Time), but
+    some do not automatically do it. In that case you should verify that <em>iat</em> &le; current
+    time &le; <em>exp</em>. You might also want to check the <em>iss</em> (Issuer) matches what you expect,
+    but this would be good to check already before fetching the keys.
   </p>
   <h3>Using the token</h3>
   <p>
     Once you've verified the token signature using the key and checked it's still valid and was
     issued by a trustworthy issuer you can safely trust the data in it. The field of primary
-    interest is the sub (Subject) which is who the user is. The aud (Audience) might be of interest
-    if you want to know through which Application on the Dataspace the user was authenticated when
-    doing the request.
+    interest is the <em>sub</em> (Subject) which is who the user is. The <em>aud</em> (Audience) might
+    be of interest if you want to know through which Application on the Dataspace the user was authenticated
+    when doing the request.
   </p>
 </TableOfContents>
