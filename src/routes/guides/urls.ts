524e2cefe2f2556e035b9e47162f378a7b9d8d85
Andrew <dyusha.ru@gmail.com>
2024-11-11 19:00:52 +0800

    remove duplicated guide, cleanup signatures guide, fix styles for Code

--- src/routes/guides/urls.ts
+++ src/routes/guides/urls.ts
@@ -1,47 +1,47 @@
 // @ts-ignore
-import QuidesIcon from "$lib/images/guides-icon.svg?dataurl"
+import GuidesIcon from "$lib/images/guides-icon.svg?dataurl"
 
 export const GUIDES = {
   BUILD_APP: {
     title: "Building an application",
     href: "/guides/building-an-application",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
   BUILD_DATA_SOURCE: {
     title: "Building a data source",
     href: "/guides/building-a-data-source",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
   BUILD_DATA_DEF: {
     title: "How to create data definitions",
     href: "/guides/building-a-data-definition",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
   VERIFY_CONSENT: {
     title: "How to verify consent in data source",
     href: "/guides/verifying-consent-in-data-source",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
   MANAGE_GROUP: {
     title: "How to create and manage a group",
     href: "/guides/managing-groups",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
   USING_LOGIN_PROVIDER: {
     title: "How to use dataspace login in an application",
     href: "/guides/using-login-provider",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
   VERIFY_ID_TOKEN: {
     title: "Verify id_token in data source",
     href: "/guides/verifying-id-token",
     cardImageAlt: "Paper sheet",
-    cardImage: QuidesIcon,
+    cardImage: GuidesIcon,
   },
 }
