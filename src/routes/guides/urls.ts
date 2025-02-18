41a2c378abbfc4d27b6a77be31e6e1e56f850404
Andrew <dyusha.ru@gmail.com>
2025-02-18 15:27:55 +0800

    verifying api token guide's object moved

--- src/routes/guides/urls.ts
+++ src/routes/guides/urls.ts
@@ -50,9 +50,6 @@ export const GUIDES = {
     cardImageAlt: "Paper sheet",
     cardImage: GuidesIcon,
   },
-}
-
-export const NOTION_GUIDES = {
   VERIFYING_API_TOKENS: {
     title: "Verifying API tokens",
     href: "https://ioxio.notion.site/Verifying-API-tokens-1528e3ae865b804c9f2dd354b143c743",
