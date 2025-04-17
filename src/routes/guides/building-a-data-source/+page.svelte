c24d8bac01f3608d616523aea42dd06c35db9d1d
meandrewandyou <73995378+meandrewandyou@users.noreply.github.com>
2025-04-17 12:48:33 +0800

    Update src/routes/guides/building-a-data-source/+page.svelte
    Co-authored-by: Joakim Nordling <joakim.nordling@gmail.com>

--- src/routes/guides/building-a-data-source/+page.svelte
+++ src/routes/guides/building-a-data-source/+page.svelte
@@ -179,7 +179,7 @@
   </p>
   <GuideImage img={images.SOURCE} />
   <p>
-    As our data source uses Dataspace verified API tokens, we'll need one before making request. You
+    As our data source uses Dataspace verified API tokens, we'll need one before making a request. You
     could generate one on <em>Access control keys</em> page.
     <GuideImage img={images.KEYS} />
   </p>
